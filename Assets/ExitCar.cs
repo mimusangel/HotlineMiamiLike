0d82c7c668a8b467afbee507966a019ec91597cd
Jean-christophe BLONDEAU <jblondea@e2r8p10.42.fr>
2018-03-31 16:48:53 +0200

    car anim + key

---
+++ Assets/ExitCar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitCar : MonoBehaviour {
+
+	public string nextScene;
+	public List<GameObject> objectsToDisable;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			PlayerPrefs.SetString("lastLoadedScene", SceneManager.GetActiveScene().name);
+			other.gameObject.SetActive(false);
+			GetComponent<Animator>().SetTrigger("Jump");
+			Camera.main.GetComponent<CameraFollowScript>().follow = gameObject;
+		}
+	}
+
+	public void DisableObjects() {
+		objectsToDisable.ForEach(item => item.SetActive(false));
+	}
+
+	public void LoadNextScene() {
+		SceneManager.LoadScene(nextScene);
+	}
+
+
+}
---
+++ Assets/key.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class key : MonoBehaviour {
+
+	public UnityEvent onCollected;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			onCollected.Invoke();
+			Destroy(gameObject);
+		}
+	}
+}
