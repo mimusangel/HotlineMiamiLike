137947c51b131409f74d4b4682d9510053991074
Jean-christophe BLONDEAU <jblondea@e2r8p10.42.fr>
2018-04-01 16:08:41 +0200

    shaker sound + background color switch

---
+++ Assets/Scripts/ColorSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSwitcher : MonoBehaviour {
+
+	private Color targetColor;
+	private float switchCooldown;
+
+	// Use this for initialization
+	void Start () {
+		switchCooldown = 0.0f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		switchCooldown -= Time.deltaTime;
+		if (switchCooldown <= 0.0f) {
+			Color c = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1);
+			targetColor = c;
+			switchCooldown = 1.0f;
+		}
+		Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor, targetColor, 0.01f);
+	}
+}
--- Assets/Scripts/Shaker.cs
+++ Assets/Scripts/Shaker.cs
@@ -23,6 +23,8 @@ public class SaveUserEditor : Editor
 
 public class Shaker : MonoBehaviour {
 
+	public AudioSource shakeSource;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -34,22 +36,30 @@ public class Shaker : MonoBehaviour {
 	}
 
 	public void Shake() {
+		shakeSource.Play();
 		StartCoroutine(ShakeRoutine());
 	}
 
 	IEnumerator ShakeRoutine() {
-		List<Vector3> shakePositions = new List<Vector3>() {
-			transform.position + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0),
-			transform.position + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0),
-			transform.position + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0),
-			transform.position + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0),
-			transform.position + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0),
-			transform.position + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0),
-			transform.position + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0),
-		};
+		List<Vector3> shakePositions = new List<Vector3>();
+		List<float> shakeStrengths = new List<float>();
+		for (int i = 0; i < 30; i++) {
+			shakePositions.Add(transform.position + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0));	
+		}
+		for (int i = 0; i < 5; i++) {
+			shakeStrengths.Add(5.0f);
+		}
+		for (int i = 0; i < 20; i++) {
+			shakeStrengths.Add(15.0f);
+		}
+		for (int i = 0; i < 5; i++) {
+			shakeStrengths.Add(3.0f);
+		}
+		int j = 0;
 		foreach(Vector3 shakePos in shakePositions) 
 		{
-			transform.position = Vector3.MoveTowards(transform.position, shakePos, 5.0f * Time.deltaTime);
+			transform.position = Vector3.MoveTowards(transform.position, shakePos, shakeStrengths[j] * Time.deltaTime);
+			j++;
 			yield return new WaitForSeconds(Random.Range(0.1f, 0.3f));
 		};
 	}
